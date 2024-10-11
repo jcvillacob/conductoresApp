bd7701c77378ae6fc65ed6562afb874dc60a0a6e
jvillacob <jvillacob@coorditanques.com>
2024-10-11 15:20:15 -0500

    feature: environmets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { AnticiposComponent } from './components/anticipos/anticipos.component';
-import { ProfileComponent } from './components/profile/profile.component';
+import { ProfileComponent } from './components/topbar/profile/profile.component';
 import { GastosComponent } from './components/gastos/gastos.component';
 import { HelpComponent } from './components/help/help.component';
 import { cedulaGuardGuard } from './cedula-guard.guard';
--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -2,4 +2,5 @@ export  const environment = {
   baseUrl: 'https://ctapp.coorditanques.com/ApiAPConductores',
   token: 'TECLABCTVV1',
   calificacionUrl: 'https://ctapp.coorditanques.com/colaboradoresBack/api/v1'
+  
 };
